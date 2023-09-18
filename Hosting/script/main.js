62d278e0432d900c60cbcc2b530bf959f88a59d1
Dhruv00421 <120349722+Dhruv00421@users.noreply.github.com>
2023-09-18 15:53:45 +0530

    Delete Hosting directory

--- Hosting/script/main.js
+++
@@ -1,8 +0,0 @@
-var back = document.getElementByid('back');
-var text = document.getElementByid('blank');
-
-text.addeventlistener("click",blur);
-
-function blur() {
-    back.classlist.blur('active');
- } 
--- Hosting/script/register.js
+++
@@ -1,2 +0,0 @@
-var fill = document.getElementsByClassName('box');
-
